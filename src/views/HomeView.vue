d07c3f78681ce060adff39f1be15a6802a8551a8
cibitungwetan <149467588+cibitungwetan@users.noreply.github.com>
2023-10-31 21:03:02 +0700

    Update visi misi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,8 +15,7 @@ import Sidebar from "../components/Sidebar.vue";
       <p class="text-3xl font-semibold">Visi & Misi Desa Cibitung Wetan</p>
       <p class="text-lg font-semibold">Visi</p>
       <p>
-        “Mewujudkan Masyarakat Desa Cibitung Wetan yang “ SERASI” 
-        (Sehat, Rapi, Aman dan Sejahtera) Pada Tahun 2029”
+        “Mewujudkan Masyarakat Desa Cibitung Wetan yang “ SERASI” (Sehat, Rapi, Aman dan Sejahtera) Pada Tahun 2029”
       </p>
       <p class="text-lg font-semibold">Misi</p>
       <ul>
