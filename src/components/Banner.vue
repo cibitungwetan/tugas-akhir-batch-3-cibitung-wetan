069794fa13c802c79aa841258b47f2e42caa7bc4
cibitungwetan <149467588+cibitungwetan@users.noreply.github.com>
2023-10-31 15:22:15 +0700

    Update link youtube

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -15,7 +15,7 @@
       <iframe
         width="480"
         height="260"
-        src="https://www.youtube.com/embed/sgekBZWvw1g?si=FuzxQ1pEq1HGYh1K"
+        src="https://www.youtube.com/embed/wDMTaLZ-XX0?si=g_X0dJ2ZgKJBPs5r"
         title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
