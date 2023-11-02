d350cdaa6772e081e420791f2ba88986fcba8890
cibitungwetan <149467588+cibitungwetan@users.noreply.github.com>
2023-11-02 12:22:05 +0700

    Update visi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,23 +12,14 @@ import Sidebar from "../components/Sidebar.vue";
     <Sidebar />
     <div class="flex-1 p-10">
       <p class="text-3xl font-semibold my-4">Visi&Misi Desa Cibitung Wetan</p>
-      <p class="text-lg font-semibold">Visi</p>
+      <p class="text-lg font-semibold my-4">Visi</p>
       <p> “Mewujudkan Masyarakat Desa Cibitung Wetan yang “ SERASI” (Sehat, Rapi, Aman dan Sejahtera) Pada Tahun 2029”
-          <p class="text-lg font-semibold MY-4">Misi</p>
-           <li>
-          1.Mewujudkan pemerintah Desa Cibitung Wetan yang efektif dan efisien dalam rangka mengoptimalkan pelayanan kepada masyarakat;
-        <li>
-          2.Meningkatkan derajat hidup masyarakat melalui upaya peningkatan pelayanan Kesehatan desa;
-         <li>
-          3.Meningkatkan pembangunan infrastruktur yang mendukung perekonomian desa;
-        <li>
-          4.Menjaga kelestarian dan sumber daya alam untuk memenuhi kebutuhan dan pemerataan guna meningkatkan perekonomian;
-        <li>
-          5.Mewujudkan perekonomian yang memiliki daya saing dan berkelanjutan;
-        </li>
-       
-        </li>
-      </ul>
+      <p class="text-lg font-semibold my-4">Misi</p>
+           1.Mewujudkan pemerintah Desa Cibitung Wetan yang efektif dan efisien dalam rangka mengoptimalkan pelayanan kepada masyarakat;
+           2.Meningkatkan derajat hidup masyarakat melalui upaya peningkatan pelayanan Kesehatan desa;
+           3.Meningkatkan pembangunan infrastruktur yang mendukung perekonomian desa;
+           4.Menjaga kelestarian dan sumber daya alam untuk memenuhi kebutuhan dan pemerataan guna meningkatkan perekonomian;
+           5.Mewujudkan perekonomian yang memiliki daya saing dan berkelanjutan;
     </div>
   </main>
 </template>
